9c88238d8d452895b2068707233a4b85e34ecd55
lberteh <lberteh@gmail.com>
2017-07-21 19:29:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
-  // link to the http library that comes with node
+  {
+    "vid": "20170303962",
+    "itemCode": "ITEM123",
+    "curve": "128,174,141,229,308,204,311,166", 
+    "label": "Enter your name", 
+    "defaultText": "Avetti Commerce", 
+    "maxChar": 30, 
+    "fontFamilies": ["font1", "font2", "font3"],
+    "defaultFontFamily": "font1", 
+    "fontSizes": [16, 18, 20, 22, 24], 
+    "defaultFontSize": 20 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// link to the http library that comes with node
 var http = require('http');
 
 http.createServer(function(request, response) {
